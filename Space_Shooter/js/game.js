9258936eeb41cd05b8f9d3c3e5c80e30777d7014
Mohammed Hassan Latif <42440580+MohammedHassanLatif@users.noreply.github.com>
2021-01-17 00:44:48 +0000

    Initial commit

--- Space_Shooter/js/game.js
+++ Space_Shooter/js/game.js
@@ -1,14 +1,1 @@
-var async = require("async");
-async.paraell ({
-    function (callback) {
-        console.log("one");
-        callback();
-    },
-    function (callback) {
-        console.log("two");
-        callback();
-    }
-},
-    function(err, result) {
-    console.log("finished");
-    })
+
